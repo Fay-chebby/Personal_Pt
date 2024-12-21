7a678b6a1e6aaca88e9c07c05b418aff32ee644c
Fai-chebby <chebetf034@gmail.com>
2024-12-21 21:31:46 +0300

    folder project

---
+++ personal_pt/src/Components/data/Folder.js
@@ -0,0 +1,15 @@
+const folder = {
+    id :"1",
+    name :"root",
+    isFolder:true,
+    items:{
+       {
+         id : "2",
+             name : "public",
+           isFolder: true,
+           items
+
+    }
+
+
+}
